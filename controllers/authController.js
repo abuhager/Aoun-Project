9cfb99526b8b9fb5342b1e7f71ccf5b589e40c7f
Adham <abuhager360@gmail.com>
2026-04-04 16:29:48 +0300

    fix bug in login after reset pass

--- controllers/authController.js
+++ controllers/authController.js
@@ -136,7 +136,7 @@ exports.login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        let user = await User.findOne({ email });
+let user = await User.findOne({ email }).select('+password');
         if (!user) return res.status(400).json({ msg: 'البريد الإلكتروني غير صحيح' });
         if (user.isBanned) {
             return res.status(403).json({ msg: 'هذا الحساب محظور بسبب مخالفة معايير مجتمع عون 🛑' });
