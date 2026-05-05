88087f00d42d9e5a075c7564ade4f79b3b02f31d
abuhager <abuhager360@gmail.com>
2026-05-06 02:07:17 +0300

    fix: update OTP validation length from 4 to 6 digits

--- dtos/authDto.js
+++ dtos/authDto.js
@@ -3,30 +3,27 @@ const Joi = require('joi');
 
 exports.validateRegister = (body) => {
   const schema = Joi.object({
-    name: Joi.string().min(3).max(50).required(),
-    email: Joi.string().email().required(),
+    name:     Joi.string().min(3).max(50).required(),
+    email:    Joi.string().email().required(),
     password: Joi.string().min(6).required(),
-    phone: Joi.string().optional()
+    phone:    Joi.string().optional()
   });
-
   return schema.validate(body);
 };
 
 exports.validateVerifyEmail = (body) => {
   const schema = Joi.object({
     email: Joi.string().email().required(),
-    otp: Joi.string().length(4).required()
+    otp:   Joi.string().length(6).required()
   });
-
   return schema.validate(body);
 };
 
 exports.validateLogin = (body) => {
   const schema = Joi.object({
-    email: Joi.string().email().required(),
+    email:    Joi.string().email().required(),
     password: Joi.string().required()
   });
-
   return schema.validate(body);
 };
 
@@ -34,7 +31,6 @@ exports.validateForgotPassword = (body) => {
   const schema = Joi.object({
     email: Joi.string().email().required()
   });
-
   return schema.validate(body);
 };
 
@@ -42,6 +38,5 @@ exports.validateResetPassword = (body) => {
   const schema = Joi.object({
     password: Joi.string().min(6).required()
   });
-
   return schema.validate(body);
 };
